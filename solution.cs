98c009a28e331a25ad4cb5f1241678be3e1b56fe
Kooreis <kooreis.official@gmail.com>
2024-03-15 03:35:30 -0500

    This update includes the prompt for the user to enter an arithmetic expression. The input is then processed to extract the operator and the operands. The operator is found using a regular expression and the operands are found by splitting the input string at the operator and converting the resulting strings to numbers.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+rl.question('Enter an arithmetic expression: ', (input) => {
+    const operator = input.match(/[\+\-\*\/]/)[0];
+    const operands = input.split(operator).map(Number);
